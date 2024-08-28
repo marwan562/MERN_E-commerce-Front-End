7f1e35b8e9af0c8b020ff306bd89fcf6258587f2
marwan.hassan <mixing.gamer546@gmail.com>
2024-08-28 12:37:38 +0300

    manage products for admin

--- src/app/(admin)/dashboard/categories/page.tsx
+++ src/app/(admin)/dashboard/categories/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { PlusCircle } from "lucide-react";
+import { PlusCircle, PlusIcon, PlusSquare } from "lucide-react";
 import CategoriesList from "../../components/CategoriesList";
 import {
   useCreateCategoryMutation,
@@ -22,6 +22,14 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { toast } from "sonner";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Component() {
   const token = useAuthToken();
@@ -75,32 +83,40 @@ export default function Component() {
     <main className="flex-1 p-4 w-full">
       <div className="grid flex-1 gap-4">
         <div className="flex items-center gap-4">
-          <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
-            Product Categories
-          </h1>
+          <Card className=" w-full">
+            <CardHeader className=" flex  flex-row items-center justify-between">
+              <CardTitle className=" underline-offset-2">
+                {" "}
+                Product Categories
+              </CardTitle>
+              <CardTitle>
+                {" "}
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button size="default">
+                      <PlusIcon className="h-5 w-5 mr-2" />
+                      Create Category
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                      <DialogTitle>Edit Category</DialogTitle>
+                      <DialogDescription>
+                        Make changes to your Category here. Click save when you
+                        {"'"}re done.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <FormCategory
+                      titleSubmit="Create New Category"
+                      isLoading={isCreating}
+                      onSave={handleCreateCategory}
+                    />
+                  </DialogContent>
+                </Dialog>
+              </CardTitle>
+            </CardHeader>
+          </Card>
           {/* Dailog With Fomr To Edit Category */}
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button size="sm">
-                <PlusCircle className="h-4 w-4 mr-2" />
-                Create Category
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Edit Category</DialogTitle>
-                <DialogDescription>
-                  Make changes to your Category here. Click save when you{"'"}re
-                  done.
-                </DialogDescription>
-              </DialogHeader>
-              <FormCategory
-                titleSubmit="Create New Category"
-                isLoading={isCreating}
-                onSave={handleCreateCategory}
-              />
-            </DialogContent>
-          </Dialog>
         </div>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
           {data?.map((category) => (

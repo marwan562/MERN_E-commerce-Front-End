9a89a8bba2865bc103c80e7df8a291c212c018ed
marwan.hassan <mixing.gamer546@gmail.com>
2024-08-30 09:24:35 +0300

    handle middleware request

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,37 +7,39 @@ const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/profile(.*)"]);
 
 export default clerkMiddleware(async (auth, req: NextRequest) => {
   if (isProtectedRoute(req)) {
-    try {
-      const { getToken } = auth();
-      const token = await getToken();
-      const response = await fetch(
-        `${process.env.BASE_URL}/protected-endpoint/createUser`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          mode: "cors",
+    const { getToken } = auth();
+    const token = await getToken();
+    if (token) {
+      try {
+        const response = await fetch(
+          `${process.env.BASE_URL}/protected-endpoint/createUser`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            mode: "cors",
+          }
+        );
+
+        if (!response.ok) {
+          console.log(`HTTP error! Status: ${response.status}`);
+          throw new Error("Failed to create user");
         }
-      );
 
-      if (!response.ok) {
-        console.log(`HTTP error! Status: ${response.status}`);
-        throw new Error("Failed to create user");
+        const user = (await response.json()) as User;
+        console.log("User response:", user);
+
+        if (user.role === "admin") {
+          return NextResponse.redirect(new URL("/dashboard", req.url));
+        } else if (user.role === "user") {
+          return NextResponse.redirect(new URL("/", req.url));
+        }
+      } catch (error) {
+        console.error("Error in middleware:", error);
+        return NextResponse.redirect(new URL("/internet", req.url));
       }
-
-      const user = (await response.json()) as User;
-      console.log("User response:", user);
-
-      if (user.role === "admin") {
-        return NextResponse.redirect(new URL("/dashboard", req.url));
-      } else if (user.role === "user") {
-        return NextResponse.redirect(new URL("/", req.url));
-      }
-    } catch (error) {
-      console.error("Error in middleware:", error);
-      return NextResponse.redirect(new URL("/internet", req.url));
     }
   }
 
